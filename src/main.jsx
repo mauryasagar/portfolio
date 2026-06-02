2d7c43a4d04d479b5ae872644f38488b02e2e405
mauryasagar <sagarmaurya010@gmail.com>
2026-06-02 20:18:16 +0530

    fix: enhance console for better engagement

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/App.css';
 
+console.log(`%cWait, you're not actually hacking. That's just console.log
+
+You're curious. I like that.
+Let's be friends →
+
+github.com/mauryasagar`, "color: #cba6f7; font-size: 14px;");
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 		<App />
