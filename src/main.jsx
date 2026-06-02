cb3e89eddf1113f11d9f2ea5a882be0d2ba8baea
mauryasagar <sagarmaurya010@gmail.com>
2026-06-02 20:49:47 +0530

    fix: update console log URL

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ console.log(
 You're curious. I like that.
 Let's be friends ↓
 
-github.com/mauryasagar`, 
+https://github.com/mauryasagar`, 
   "color: #00FFFF; font-size: 16px; text-shadow: 0 0 4px #00FFFF;"
 );
 
