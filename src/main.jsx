843f45d5730eaecc8582890c6c07634d671ace1c
mauryasagar <sagarmaurya010@gmail.com>
2026-06-02 20:43:22 +0530

    fix: update console

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/App.css';
 
-console.log(`%cWait, you're not actually hacking. That's just console.log
+console.log(
+  `%cWait, you're not actually hacking. That's just console.log
 
 You're curious. I like that.
-Let's be friends →
+Let's be friends ↓
 
-github.com/mauryasagar`, "color: #cba6f7; font-size: 14px;");
+github.com/mauryasagar`, 
+  "color: #00FFFF; font-size: 14px; text-shadow: 0 0 0px #00FFFF;"
+);
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
